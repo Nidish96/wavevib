1423e05afc9d5cd8a36455ca8c914a1466fee366
nidish <nidbid@gmail.com>
2023-04-26 20:12:09 +0200

    Updated AFT.m to more efficient version

--- ROUTINES/WBM/AFT.m
+++ ROUTINES/WBM/AFT.m
@@ -4,6 +4,8 @@ function [yout] = AFT(yin, h, Nt, dir)
 %frequency-time ('f2t') transformations.
 %
 %   USAGE :
+%       youts = AFT(yin, h, Nt, dir);
+%   INPUTS :
 %       yin     : 't2f'(Nt^Nc, Ny) or 'f2t'(Nhc,Ny)
 %       h       : (Nh,Nc)
 %       Nt      :
@@ -27,48 +29,85 @@ function [yout] = AFT(yin, h, Nt, dir)
     
     if strcmp(dir, 't2f')
         yout = zeros(Nhc, Ny);
-        for yi=1:Ny
-            yt = reshape(yin(:, yi), [repmat(Nt, 1, Nc) ones(1, Nc==1)]);
-            
-            % Fourier Transform & Scale
-            yf = fftshift(fftn(yt))*2/(Nt^Nc);
-            yf(i0{:}) = yf(i0{:})/2;
-    
-            % Choose selected Harmonics
+        if Ny<Nc    % Option 1 - Loop over Ny
+            for yi=1:Ny
+                yf = reshape(yin(:, yi), [repmat(Nt, 1, Nc) ones(1, Nc==1)]);
+                
+                % Fourier Transform & Scale
+                yf = fftshift(fftn(yf))*2/(Nt^Nc);
+                yf(i0{:}) = yf(i0{:})/2;
+        
+                % Choose selected Harmonics
+                k = 1;
+                for hi=1:Nh
+                    if all(h(hi,:)==0)  % zero harmonics
+                        yout(k,yi) = yf(i0{:});
+                        k = k+1;
+                    else
+                        id = num2cell(iof+h(hi,:)); % Focus for optimization
+                        yout(k,yi)   = real(yf(id{:}));
+                        yout(k+1,yi) = -imag(yf(id{:}));
+                        k = k+2;
+                    end
+                end
+            end
+        else    % Option 2 - Do 1D fft over each direction
+            yf = reshape(yin, [repmat(Nt, 1, Nc) Ny]);
+            if Nc==2
+                yf = fftshift(fftshift(fft2(yf),1),2);
+            else
+                for ic=1:Nc
+                    yf = fftshift(fft(yf, [], ic),ic);
+                end
+            end
+            yf = yf*2/(Nt^Nc);
+            yf(i0{:}, :) = yf(i0{:}, :)/2;
+            % Choose Selected Harmonics
             k = 1;
             for hi=1:Nh
-                if all(h(hi,:)==0)  % zero harmonics
-                    yout(k,yi) = yf(i0{:});
+                if all(h(hi,:)==0)
+                    yout(k,:) = yf(i0{:}, :);
                     k = k+1;
                 else
-                    id = num2cell(iof+h(hi,:)); % Focus for optimization
-                    yout(k,yi)   = real(yf(id{:}));
-                    yout(k+1,yi) = -imag(yf(id{:}));
+                    id = num2cell(iof+h(hi,:));
+                    yout(k, :) = real(yf(id{:}, :));
+                    yout(k+1, :) = -imag(yf(id{:}, :));
                     k = k+2;
                 end
             end
         end
     elseif strcmp(dir, 'f2t')
         yout = zeros(Nt^Nc,Ny);
-        for yi=1:Ny  % can be task-parallelized
-            yf = zeros([repmat(Nt, 1, Nc) ones(1,Nc==1)]);
-            k = 1;
-            for hi=1:Nh
-                if all(h(hi,:)==0)
-                    yf(i0{:}) = yin(k,yi)*2;
-                    k = k+1;
-                else
-                    id = num2cell(iof+h(hi,:)); % Focus for optimization
-                    yf(id{:}) = yin(k,yi)-1j*yin(k+1,yi);
-                    id = num2cell(iof-h(hi,:));
-                    yf(id{:}) = yin(k,yi)+1j*yin(k+1,yi);
-                    k = k+2;
+        yf = zeros([repmat(Nt, 1, Nc) Ny]);
+        k = 1;
+        for hi=1:Nh
+            if all(h(hi,:)==0)
+                yf(i0{:},:) = yin(k,:)*2;
+                k = k+1;
+            else
+                id = num2cell(iof+h(hi,:));
+                yf(id{:}, :) = yin(k,:)-1j*yin(k+1,:);
+                id = num2cell(iof-h(hi,:));
+                yf(id{:}, :) = yin(k,:)+1j*yin(k+1,:);
+                k = k+2;
+            end
+        end
+        yf = yf*(Nt^Nc)/2;
+
+        if Ny<Nc
+            % IFFT
+            for yi=1:Ny  % can be task-parallelized
+                yout(:, yi) = reshape(ifftn(ifftshift(yf)), Nt^Nc, 1);
+            end
+        else    % Option 2 - Do 1D fft over each direction
+            if Nc==2
+                yf = ifft2(ifftshift(ifftshift(yf,1),2));
+            else
+                for ic=1:Nc
+                    yf = ifft(ifftshift(yf,ic), [], ic);
                 end
             end
-            yf = yf*(Nt^Nc)/2;
-    
-            % IFFT
-            yout(:, yi) = reshape(ifftn(ifftshift(yf)), Nt^Nc, 1);
+            yout = real(reshape(yf, Nt^Nc, Ny));
         end
     end
 end
